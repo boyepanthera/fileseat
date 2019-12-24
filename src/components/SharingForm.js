281df9175b3103f3b5da388c163e1ecb6de054a5
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2019-12-24 05:39:35 +0100

    writing dropzone

--- src/components/SharingForm.js
+++ src/components/SharingForm.js
@@ -28,7 +28,7 @@ const SharingForm = ()=> {
             onSubmit={handleSubmit}
             initialValues = {{
               receipientEmail : '', message: '',
-              senderEmail : '', file: ''
+              senderEmail : '', files:  []
             }}      
             >
               {
@@ -55,19 +55,39 @@ const SharingForm = ()=> {
                     </div>
                     <div  className='form-group'>
                       <label htmlFor='message'>Message:</label>
-                      <Field name='message' className='Message-Input form-control' row='5' id='message' component='textarea'/>
+                      <Field name='message' className='Message-Input form-control' row='7' id='message' component='textarea'/>
                     </div>
-                    <Dropzone>
+                    {/* <Dropzone>
                       <div className='form-group '>
                           <label className='Upload-Div' htmlFor='file'> 
                             <input type='file' style={{display:'none'}} id='file' name='file' onChange={(event) => {
                                 setFieldValue('file', event.currentTarget.files[0])
                               }} multiple />
-                            <div className='text-center'><CloudUpload >
+                            <div className='text-center'>
+                            <CloudUpload >
                             </CloudUpload><p>Drag and drop here or browse files</p></div>
                           </label>
                       </div>
-                    </Dropzone>            
+                    </Dropzone>             */}
+                    <Dropzone multiple onDrop={(acceptedFiles)=>{
+                      // Do nothing if files is not yet dropped
+                      if(acceptedFiles.length===0) {return null}
+                      // send files values to formik state if it exists
+                      setFieldValue('files', values.files.concat(acceptedFiles))
+                    }}>
+                        {
+                          ({isDragActive, isDragReject}) => {
+                           if(isDragActive ){
+                              return 'This file is allowed'
+                            }  else if (isDragReject){
+                              return 'This file extension is not allowed'
+                            } else if (files.values.length===0) {
+                              return 'Drag and drop files here or click to browse files'
+                            }
+                            return values.files.map((file, i) => (<p key={i} file={file} />));
+                          }
+                        }
+                    </Dropzone>
                     <button type='submit' onClick={handleSubmit} className='Transfer-Button col'>Transfer</button>
                   </Form>
                 
