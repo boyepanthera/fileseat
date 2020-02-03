f6181d10f66259ae1be0c5bde3925948be994951
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-03 02:58:18 +0100

    purge bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import {NavLink} from 'react-router-dom';
 import '../assets/css/Navbar.css';
 const Navbar =  ()=>  {
     return (
-        <nav className='Navbar'>
+        <nav className='justify-between'>
             <div>                
-                <NavLink className='NavLink text-white' to='/'>FS</NavLink>  
+                <NavLink className='NavLink text-white font-bold' to='/'>FS</NavLink>  
             </div>
             <div>
-                <NavLink className='text-white m-1' to='/auth'><button className='bg-white'>SignIn</button></NavLink>                  
-                <NavLink className='text-white m-1' to='/about'>About</NavLink>                  
-                <NavLink className='text-white m-1' to='/help'>Help</NavLink> 
+                <NavLink className='-m-1' to='/about'><button className='bg-white rounded-l-lg font-bold px-2 py-1'>About</button></NavLink>                  
+                <NavLink className='-m-1' to='/auth'><button className='bg-white font-bold px-2 py-1'>SignIn</button></NavLink>                  
+                <NavLink className='-m-1' to='/help'><button className='bg-white font-bold px-2 py-1 rounded-r-lg'>Help</button></NavLink> 
             </div>                 
         </nav>
     )
--- src/components/SharingForm.js
+++ src/components/SharingForm.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {Formik, Form, Field} from 'formik';
-import '../assets/css/SharingForm.css';
 import {CloudUpload} from '@material-ui/icons';
 import Dropzone from 'react-dropzone';
 import {Typography} from '@material-ui/core'; 
@@ -16,8 +15,7 @@ const SharingForm = ()=> {
     }  
 
     return ( 
-            <div className='row'>
-              <div className='col-12'>
+      <div className='w-full max-w-sm'>
               <Formik 
             onSubmit={handleSubmit}
             initialValues = {{
@@ -37,27 +35,28 @@ const SharingForm = ()=> {
                   isSubmitting, setFieldValue
                 }
                 )=> (              
-                  <Form className='SharingForm' onSubmit={handleSubmit} >
-                    <Typography variat='h2' className='text-center form-header' component='h2'>TRANSFER FILES</Typography>
-                    <div className='form-group'>
-                      <label htmlFor='receipientEmail' className='labels'>Send files to this email:</label>
-                      <Field type='email' className='Form-Input' name='receipientEmail' id='receipientEmail'/>
+                    <Form className='rounded-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 my-20' onSubmit={handleSubmit} >
+                    <div>
+                        <Typography variat='h2' className='text-center my-10' component='h2'>TRANSFER FILES</Typography>
+                    </div>
+                    <div className='mb-4'>
+                        <label htmlFor='receipientEmail' className='block text-gray-700 text-sm font-bold mb-2 mt-8'>Send files to this email:</label>
+                        <Field type='email' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-gray-400 ' name='receipientEmail' id='receipientEmail'/>
                     </div>
                     <div  className='form-group'>
-                      <label htmlFor='senderEmail'>Your email:</label>
-                      <Field type='email' className='Form-Input' name='senderEmail' id='senderEmail' />
+                        <label htmlFor='senderEmail' className='block text-gray-700 text-sm font-bold mb-2'>Your email:</label>
+                        <Field type='email' className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-gray-400 ' name='senderEmail' id='senderEmail' />
                     </div>
-                    <div  className='form-group'>
-                      <label htmlFor='message'>Message:</label>
-                      <Field name='message' className='Message-Input form-control' row='7' id='message' component='textarea'/>
+                    <div  className='mb-6'>
+                        <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='message'>Message:</label>
+                        <Field name='message' className=' border-gray-400 appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none' row='7' id='message' component='textarea'/>
                     </div>
-                    <button type='submit' onClick={handleSubmit} className='Transfer-Button col'>Transfer</button>
+                    <button type='submit' onClick={handleSubmit} className='bg-blue-700 rounded-lg text-white font-bold p-2'>Transfer</button>
                   </Form>     
                 )
               }
             </Formik>
               </div>
-            </div>
     )
 }
 
