68284fbec2ed3d1bf386f0cdce0b343f29841e1c
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-05 00:00:28 +0100

    login form style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,54 @@
 import React from 'react';
-import Navbar from './Navbar';
 import {Formik, Form, Field} from 'formik';
 import LoginPicture from '../assets/images/ThreeHappyFriends.jpg';
 import styled from 'styled-components';
+import * as Yup from 'yup';
+import {Link} from 'react-router-dom';
 
 const ErrMessage = styled.div`
     color:  red
 `
+const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+    .email('This is not a valid email'),
+    password: Yup.string()
+    .min(8, 'Your password cannot be fewer than 8 characters')
+})
+
 
 const Login = ()=> {
     return (
 
         <div className='h-full'>
-            <div className='w-1/2'>
+            <div className='w-1/2 m-0'>
                 <Formik
                 initialValues = 
                 {{email : '' , password : ''}}
-                
+                validationSchema={LoginSchema}
+                onSubmit={values=> console.log(values)}
                 >
                 {props => (
-                    <Form className='w-full max-w-sm rounded-full'>
-                        <h1>Welcome Back</h1>
-                        <div>Login to your account</div>
-                        <div className='mb-4'>
-                            <label htmlFor='email'>Email</label>
-                            <Field name='email' className='border-b-2 border-indigo-700 w-full' type='email' />
+                    <Form className='w-full max-w-sm rounded-lg bg-white p-12 my-20 shadow-lg'>
+                        <h1 className='text-center text-2xl font-extrabold mb-4'>Welcome Back</h1>
+                        <div className='text-center font-bold'>Login to your account</div>
+                        <div className='mb-4 mt-12'>
+                                <label className='font-black' htmlFor='email'>Email</label>
+                            <Field name='email' className='border-b-2 h-10 border-indigo-700 w-full' type='email' />
+                            {props.errors.name && props.touched.name ? <ErrMessage/>: null}
                         </div>
-                        <div className='mb-6'>
-                            <label htmlFor='password'>Password</label>
-                            <Field name='password' className='border-b-2 border-indigo-700 w-full' type='password' />
+                        <div className='mb-8'>
+                            <label className='font-black' htmlFor='password'>Password</label>
+                            <Field name='password' className='border-b-2 h-10 border-indigo-700 w-full' type='password' />
+                            {props.errors.password && props.touched.password ? <ErrMessage /> : null}
                         </div>
-                        <button type='submit rounded-full w-full hover:bg-indigo-700'>Login</button>
+                        <button type='submit' className='rounded-full bg-indigo-700 w-full hover:bg-indigo-700 text-white p-2'>Login</button>
+                        <div className='m-4 text-center'>Forgot password?</div>
+                        <div className='m-6 text-center'>Dont have an account yet? <Link className='text-purple-500' to='/newauth'>Sign Up</Link></div>
                     </Form>
                 )}
                 </Formik>
             </div>
             <div className='w-1/2'>
-                <Navbar/>
                 <image src={LoginPicture} alt='Three happy friends in the snow'/>
             </div>
 
