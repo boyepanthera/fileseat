316757dc81fc03b198836e70cbd116686d5359e2
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-04 17:57:41 +0100

    resp form

--- src/components/SharingForm.js
+++ src/components/SharingForm.js
@@ -14,7 +14,7 @@ const SharingForm = ()=> {
     }  
 
     return ( 
-      <div className='w-full max-w-sm container min-w-sm'>
+      <div className='min-w-sm container w-1/4'>
               <Formik 
             onSubmit={handleSubmit}
             initialValues = {{
