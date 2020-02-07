6a237dd8ccf25b40a5b668b4454123907c5e4f3f
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-07 23:11:37 +0100

    protected route creation

---
+++ src/components/Dashboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const UserDashboard = ()=> 
+    (
+        <div className='m-0 h-full bg-gray-200'>
+            <div className='text-bold'>Hey I am a protected Route
+                <div>Users can use me to</div>
+                <ul>
+                    <li>Know the number of files they have shared all time!</li>
+                    <li>Know the dates they uploaded and the date of deletion/expiration</li>
+                    <li>List of file deprecated</li>
+                    <li>List of file Active</li>
+                </ul>
+            </div>
+        </div>
+    )
+
+export const AdminDashboard = ()=> 
+    (
+        <div className='m-0 h-full bg-gray-200'>
+            <div>
+                <div className='text-bold'>Hey I am a protected Route for Admin</div>
+                <div>Admin can use me to</div>
+                    <ul>
+                        <li>Know the number Senders all time</li>
+                        <li>List of Registered Senders</li>
+                        <li>List of Anonymous Senders</li>
+                        <li>List of Registered Receipientss</li>
+                        <li>List of Anonymous Receipientss</li>
+                        <li>List of all files sent all time</li>
+                        <li>List of files based on category Deprecated/Active</li>
+                    </ul>
+                </div>
+        </div>
+    )
