cfe4e26366272f8180b67d66a8285318446ff18c
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-14 00:31:19 +0100

    dropzone

--- src/components/Fileseat.js
+++ src/components/Fileseat.js
@@ -1,86 +1,151 @@
-import React from 'react';
-import axios from 'axios';
-import {Formik, Form, Field} from 'formik';
-import BackgroundImage from '../assets/images/bg.png';
-import {Navbar} from './Navbar';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import Dropzone from 'react-dropzone';
+import React, { useState } from "react";
+import axios from "axios";
+import { Formik, Form, Field } from "formik";
+import BackgroundImage from "../assets/images/bg.png";
+import { Navbar } from "./Navbar";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import { useDropzone } from "react-dropzone";
 
-const LoginStyles = {background : { 
-  backgroundImage: "url(" + BackgroundImage + ")", 
-  backgroundSize: "cover", 
-  backgroundRepeat: "no-repeat",
-}}
+const LoginStyles = {
+  background: {
+    backgroundImage: "url(" + BackgroundImage + ")",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+  }
+};
 
-const Fileseat = ()=> {
-    const handleSubmit = (values)=> {
-      console.log(values)
-      axios.post('http://localhost:3001/api/v1/file/new', values, {
-        headers: {"Accept": "multipart/form-data"}
+const Fileseat = () => {
+  const [fileData, setFileData] = useState(false);
+  const handleSubmit = values => {
+    console.log(values);
+    axios
+      .post("http://localhost:3001/api/v1/file/new", values, {
+        headers: { Accept: "multipart/form-data" }
       })
-      .then (response=> console.log(response))
-    }  
+      .then(response => console.log(response));
+  };
 
-    return ( 
-      <div className='m-0 p-16' style={LoginStyles.background}>
-        <Navbar/>
-        <div className='min-w-sm sm:mx-2 container w-1/4'>s
-          <Formik
-            onSubmit={handleSubmit}
-            initialValues={{
-              receipientEmail: '', message: '',
-              senderEmail: '', files: []
-            }}
-          >
-            {
-              (
-                {
-                  values,
-                  errors,
-                  touched,
-                  handleChange,
-                  handleBlur,
-                  handleSubmit,
-                  isSubmitting, setFieldValue
-                }
-              ) => (
-                  <Form className='rounded-larger bg-white shadow-lg rounded px-10 pt-6 pb-8 my-8' onSubmit={handleSubmit} >
-                    <div>
-                      <h3 className='text-center text-xl font-bold tracking-normal'>TRANSFER FILES</h3>
-                    </div>
-                    <div className='mb-4'>
-                      <label htmlFor='receipientEmail' className='block text-gray-700 text-sm font-bold mb-2 mt-8'>Send files to this email:</label>
-                      <Field type='email' className='bg-indigo-100 focus:bg-white shadow-sm appearance-none border-b-2  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-500 ' name='receipientEmail' id='receipientEmail' />
-                    </div>
-                    <div className='mb-2'>
-                      <label htmlFor='senderEmail' className='block text-gray-700 text-sm font-bold mb-2'>Your email:</label>
-                      <Field type='email' className='bg-indigo-100 focus:bg-white shadow-sm appearance-none border-b-2 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-blue-500 ' name='senderEmail' id='senderEmail' />
-                    </div>
-                    <div className='mb-6'>
-                      <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='message'>Message:</label>
-                      <Field name='message' className='bg-indigo-100 focus:bg-white shadow-sm appearance-none border-b-2 border-blue-500  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' row='7' id='message' component='textarea' />
-                    </div>
-                    <Dropzone onDrop = {droppedFiles=> console.log(droppedFiles)} maxSize='125000000'>
-                      {
-                        ({getRootProps, getInputProps})=> 
-                          <div className='border-dashed border-gray-600 border-2 h-24 mb-4' {...getRootProps()}>
-                            <div className='mx-32 my-8 text-indigo-700' >
-                              <input {...getInputProps()}/>
-                              <CloudUploadIcon color='inherit' fontSize='large' />
-                            </div>
-                          </div>
-                        
+  const { getRootProps, getInputProps } = useDropzone();
+
+  // const files = droppedFiles.map(file => <span>{file.name}</span>);
+  return (
+    <div className="m-0 p-16" style={LoginStyles.background}>
+      <Navbar />
+      <div className="min-w-sm sm:mx-2 container w-1/4">
+        s
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={{
+            receipientEmail: "",
+            message: "",
+            senderEmail: "",
+            files: []
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            setFieldValue
+          }) => (
+            <Form
+              className="rounded-larger bg-white shadow-lg rounded px-10 pt-6 pb-8 my-8"
+              onSubmit={handleSubmit}
+            >
+              <div>
+                <h3 className="text-center text-xl font-bold tracking-normal">
+                  TRANSFER FILES
+                </h3>
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="receipientEmail"
+                  className="block text-gray-700 text-sm font-bold mb-2 mt-8"
+                >
+                  Send files to this email:
+                </label>
+                <Field
+                  type="email"
+                  className="bg-indigo-100 focus:bg-white shadow-sm appearance-none border-b-2  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-500 "
+                  name="receipientEmail"
+                  id="receipientEmail"
+                />
+              </div>
+              <div className="mb-2">
+                <label
+                  htmlFor="senderEmail"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Your email:
+                </label>
+                <Field
+                  type="email"
+                  className="bg-indigo-100 focus:bg-white shadow-sm appearance-none border-b-2 w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border-blue-500 "
+                  name="senderEmail"
+                  id="senderEmail"
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="message"
+                >
+                  Message:
+                </label>
+                <Field
+                  name="message"
+                  className="bg-indigo-100 focus:bg-white shadow-sm appearance-none border-b-2 border-blue-500  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  row="7"
+                  id="message"
+                  component="textarea"
+                />
+              </div>
+              {/* <Dropzone onDrop = {droppedFiles=> {console.log(droppedFiles);}} maxSize={125000000}> */}
+              {/* {
+                        ({getRootProps, getInputProps})=>  */}
+              <div
+                className="border-dashed border-indigo-600 border-2 h-24 mb-4"
+                {...getRootProps()}
+              >
+                <div className="mx-32 my-8 text-indigo-700">
+                  <input
+                    name="files"
+                    {...getInputProps({
+                      onChange: event => {
+                        setFieldValue("files", event.currentTarget.files);
+                        setFileData(event.currentTarget.files);
                       }
-                    </Dropzone>
+                    })}
+                  />
+                  {fileData ? (
+                    <p>
+                      {fileData[0].name} - {fileData[0].size}
+                    </p>
+                  ) : (
+                    <CloudUploadIcon color="inherit" fontSize="large" />
+                  )}
+                </div>
+              </div>
+              {/* } */}
+              {/* </Dropzone> */}
 
-                    <button type='submit' onClick={handleSubmit} className='hover:bg-indigo-500 rounded-full shadow-lg w-full bg-indigo-700 rounded-lg text-white font-bold p-2'>Transfer</button>
-                  </Form>
-                )
-            }
-          </Formik>
-        </div>
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="hover:bg-indigo-500 rounded-full shadow-lg w-full bg-indigo-700 rounded-lg text-white font-bold p-2"
+              >
+                Transfer
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Fileseat;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import Fileseat from './Fileseat';
-import Login from './Login';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import '../assets/css/Home.css';
-import {Signup} from './Signup';
-import {AdminDashboard, UserDashboard} from './Dashboard';
-import {NotFound} from './NotFound';
-import {PrivateRoute} from '../utils/auth'
+import React from "react";
+import Fileseat from "./Fileseat";
+import Login from "./Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "../assets/css/Home.css";
+import { Signup } from "./Signup";
+import { AdminDashboard, UserDashboard } from "./Dashboard";
+import { NotFound } from "./NotFound";
+import { PrivateRoute } from "../utils/auth";
 
-const Home = ()=> {
-    return (
-      <>
+const Home = () => {
+  return (
+    <>
       <Router>
-      <div  className='h-full'>
+        <div className="h-full">
           <Switch>
-            <Route exact path ='/' component={Fileseat} />         
-            <Route path ='/auth' component={Login} />         
-            <Route path ='/newauth' component={Signup} />
-            <Route path='/user' component={UserDashboard} />
-            <PrivateRoute path='/admin' component={AdminDashboard} />
+            <Route exact path="/" component={Fileseat} />
+            <Route path="/auth" component={Login} />
+            <Route path="/newauth" component={Signup} />
+            <Route path="/user" component={UserDashboard} />
+            <PrivateRoute path="/admin" component={AdminDashboard} />
             {/* <Route path='/admin' component={AdminDashboard} />    */}
-            <Route path='*' component={NotFound} />
+            <Route path="*" component={NotFound} />
           </Switch>
-       </div>       
+        </div>
       </Router>
-      </>
-    )
-}
+    </>
+  );
+};
 
 export default Home;
