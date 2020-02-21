38e2550743b6cfcfc8f05cd7a1b9ecd19f8f26cc
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-02-21 21:21:45 +0100

    full auth

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,43 +1,87 @@
-import React from "react";
+import React, { useContext, useReducer, useEffect } from "react";
 import { AuthContext } from "./Home";
 import { useHistory, Link } from 'react-router-dom';
+import axios from "axios";
+import { PageLoader } from '../utils/index';
+
+const initialState = {
+  user: null,
+  isFetching: false,
+  err: false
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_USER_DETAILS':
+      return {
+        ...state,
+        isFetching: true,
+        err: false
+      }
+    case 'FETCH_USER_SUCCESS':
+      return {
+        ...state,
+        isFetching: false,
+        user: action.payload.user
+      }
+    case 'FETCH_USER_FAILURE':
+      return {
+        ...state,
+        isFetching: true,
+        user: null
+      }
+    default:
+      return state;
+  }
+}
 
 export const UserDashboard = () => {
-  const { dispatch } = React.useContext(AuthContext);
+  const { dispatchAuth } = useContext(AuthContext);
   let history = useHistory();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    dispatch({ type: 'FETCH_USER_DETAILS' });
+    axios.get(`http://localhost:3005/api/v1/users/`)
+      .then(response => {
+        dispatch({
+          type: "FETCH_USER_SUCCESS",
+          payload: response.data.user
+        });
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch({ type: 'FETCH_USER_FAILURE' })
+      })
+  }, [dispatchAuth.token])
   return (
     <div className="m-0 bg-gray-200 p-20">
-      <div className="text-bold">
-        Hey I am a protected Route
-      <div>Users can use me to</div>
-        <button
-          className="bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:outline-none  text-white p-2"
-          onClick={() => {
-            history.push("/admin");
-          }} >
-          Logout X
-      </button>
+      {state.isFetching ? <PageLoader /> :
+        (
+          <>
+            <div>Users can use me to</div>
+            <button
+              className="bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:outline-none  text-white p-2"
+              onClick={() => {
+                dispatchAuth({ type: 'LOGOUT' });
+                history.push("/");
+              }} >
+              <span>Logout </span> <i className='fas fa-sign-out-alt'></i>
+            </button>
+            <Link className='text-white' to='/admin'>Admin</Link>
 
-        <button
-          className="bg-red-700 rounded-lg hover:bg-indigo-800 focus:outline-none  text-white p-2"
-          onClick={() => {
-            dispatch({ type: 'LOGOUT' })
-            history.push("/");
-          }} >
-          Logout X
-      </button>
-        <Link className='text-white' to='/admin'>Admin</Link>
+            <ul>
+              <li>Know the number of files they have shared all time!</li>
+              <li>
+                Know the dates they uploaded and the date of deletion/expiration
+              </li>
+              <li>List of file deprecated</li>
+              <li>List of file Active</li>
+            </ul>
+          </>
+        )
+      }
 
-        <ul>
-          <li>Know the number of files they have shared all time!</li>
-          <li>
-            Know the dates they uploaded and the date of deletion/expiration
-        </li>
-          <li>List of file deprecated</li>
-          <li>List of file Active</li>
-        </ul>
-      </div>
     </div>
   )
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,9 @@ export const ErrMessage = styled.div`
 `;
 
 export const Spinner = () => <span>Signing In... <i className='fas fa-spinner fa-pulse'></i></span>
+
+export const PageLoader = () =>
+  <div>
+    <div className='text-purple-800 text-4xl'> Loading... </div>
+    <i className='fas fa-spinner fa-pulse text-2xl'></i>
+  </div>
