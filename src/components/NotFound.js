ff358a11378c5275e5d4fbce21fd1aaf532f7ae1
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-03-13 00:37:16 +0100

    not found resp v3

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -12,8 +12,8 @@ const styles = {
 export const NotFound = () => (
     <div className='m-0 bg-gray-300 h-full py-8 px-4 sm:p-12'>
         <NavbarColor />
-        <div className='py-20 m-20'>
-            <div className='text-center text-semibold text-2xl font-semibold sm:text-6xl'>Ooops! Even we are looking for this page.</div>
+        <div className='py-16 m-4 sm:py-20 sm:m-20'>
+            <div className='text-center text-semibold text-3xl font-semibold sm:text-6xl'>Ooops! Even we are looking for this page.</div>
             <img src={PageNotFound} styles={styles.errImage} className='mx-auto p-12 h-64' alt='Error 404, Page not found illustration' />
         </div>
 
