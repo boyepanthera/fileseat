1cef0e2602beb86d016376897ae4d6735d4e265e
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-03-13 00:25:23 +0100

    resp home -v5

--- src/components/Fileseat.js
+++ src/components/Fileseat.js
@@ -72,7 +72,7 @@ const Fileseat = () => {
         <title>Fileseat, easy and fast way to share large files.</title>
         <meta name="description" content="Sometimes we want to share those files that are way above 50mb then our mail service tell us that is not possible to attach, attachment and mails? Fileseat can handle it all for you." />
       </Helmet>
-      <div className="h-screen p-16 sm:p-20" style={LoginStyles.background}>
+      <div className="h-screen p-8 sm:p-20" style={LoginStyles.background}>
         <Navbar />
         {err ? (<div className="text-red-800 w-1/4 ml-20 bg-red-100 my-2 border py-1 border-red-300 rounded-lg mx-auto  text-center text-red-500">{err.message}</div>) : null}
         <div className="sm:mx-2">
