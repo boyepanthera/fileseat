a315380101cf044a301d1a353605b51ba9d1e96b
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-06-05 10:38:47 +0100

    abt pg resp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,7 +71,7 @@ export const ResetNavbar = () => {
 export const NavbarColor = () => {
   return (
     <div className="flex justify-between">
-      <div className="w-1/2">
+      <div className="">
         <NavLink
           className="text-purple-900 font-extrabold tracking-wider ml-6 text-4xl"
           to="/"
@@ -81,6 +81,41 @@ export const NavbarColor = () => {
       </div>
       <div className="w-1/2 mt-4">
         <div className="float-right mr-10">
+          <NavLink className="mr-1" to="/about">
+            <button className="bg-white rounded-l-lg font-bold px-2 py-1  ">
+              About
+            </button>
+          </NavLink>
+          <NavLink className="mr-1" to="/newauth">
+            <button className="bg-white font-bold px-2 py-1">SignUp</button>
+          </NavLink>
+          <NavLink className="mr-1" to="/auth">
+            <button className="bg-white font-bold px-2 py-1">SignIn</button>
+          </NavLink>
+          <NavLink className="" to="/help">
+            <button className="bg-white font-bold px-2 py-1 rounded-r-lg ">
+              Help
+            </button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const NavbarAbout = () => {
+  return (
+    <div className="flex  justify-between">
+      <div className="">
+        <NavLink
+          className="text-purple-900 font-extrabold tracking-wider ml-6 text-4xl"
+          to="/"
+        >
+          FS
+        </NavLink>
+      </div>
+      <div className="">
+        <div className="">
           <NavLink className="mr-1" to="/about">
             <button className="bg-white rounded-l-lg font-bold px-2 py-1  ">
               About
